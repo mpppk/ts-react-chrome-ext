7cec86710a80bbffa856e7e57c216871117c73ef
mpppk <niboshiporipori@gmail.com>
2020-08-02 17:05:00 +0900

    Use esbuild

--- src/content_script.tsx
+++ src/content_script.tsx
@@ -1,15 +1,8 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
-import * as serviceWorker from './serviceWorker';
 
 const root = document.createElement('div');
 (root as any).style = "position: absolute; top: 0; bottom: 0; left: 0; right: 0; z-index: 1000000";
 document.body.appendChild(root);
-ReactDOM.render(<App />, root);
+ReactDOM.render(<h1>React is here!</h1>, root);
 
-// If you want your app to work offline and load faster, you can change
-// unregister() to register() below. Note this comes with some pitfalls.
-// Learn more about service workers: https://bit.ly/CRA-PWA
-serviceWorker.unregister();
---
+++ src/options_page.tsx
@@ -0,0 +1,5 @@
+import React from 'react';
+import ReactDOM from 'react-dom';
+
+ReactDOM.render(<h1>React is here!</h1>, document.querySelector('#root'));
+
